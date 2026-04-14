feca68711d098bfbd70295bf011514f984fbe867
Bohdan <blazarchuk@gmail.com>
2026-04-14 20:43:55 +0200

    Add data sending script

---
+++ js/contact-form.js
@@ -0,0 +1,33 @@
+const contactForm = document.querySelector(".contact-form");
+
+contactForm.addEventListener("submit", async (e) => {
+  e.preventDefault(); // Зупиняємо стандартне перезавантаження сторінки
+
+  // Збираємо дані з полів форми
+  const formData = {
+    name: document.getElementById("user-name").value,
+    email: document.getElementById("user-email").value,
+    phone: document.getElementById("user-phone").value,
+    description: document.getElementById("beschreibung").value,
+  };
+
+  try {
+    const response = await fetch("http://localhost:8080/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      alert("Дякуємо! Ваша заявка успішно надіслана.");
+      contactForm.reset(); // Очищуємо форму
+    } else {
+      alert("Помилка при відправці. Спробуйте пізніше.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Не вдалося з’єднатися з сервером.");
+  }
+});
